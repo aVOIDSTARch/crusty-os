322085dfec30bf547dab78c4707ffa47396b638a
louisc <lcasine@my.wgu.edu>
2026-01-05 11:04:22 -0500

    initial commit

---
+++ crusty-os/src/main.rs
@@ -0,0 +1,14 @@
+// src/main.rs
+
+#![no_std]
+
+
+use core::panic::PanicInfo;
+
+/// This function is called on panic.
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+fn main() {}
