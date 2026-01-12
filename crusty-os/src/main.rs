f90dcea3cb818cdadffb50a5f1287d94ec0b1322
louisc <lcasine@my.wgu.edu>
2026-01-12 12:07:57 -0500

    vga-buffer implemented

--- crusty-os/src/main.rs
+++ crusty-os/src/main.rs
@@ -10,17 +10,14 @@ static HELLO: &[u8] = b"Hello World!";
 
 #[unsafe(no_mangle)] // don't mangle the name of this function
 pub extern "C" fn _start() -> ! {
-    // this function is the entry point, since the linker looks for a function
-    // named `_start` by default
+   println!("Hello World{}", "!");
 
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    vga_buffer::print_something();
     loop {}
 }
 
 /// This function is called on panic.
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
